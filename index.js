280b68eab1f4be68e32a0a950d47fde1ad44b18b
Abhishek Choudhary <abhi@Abhisheks-MacBook-Pro.local>
2021-11-23 19:36:26 +0530

    intro of index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', function (req, res) {
+  res.send('Hello World, How are you')
+})
+
+app.listen(port, ()=>{console.log(`I got statred on port = ${port}`)})
+
+
+
+
+
+
+
+
+
+// const express = require('express')
+// const app = express()
+ 
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+ 
+// app.listen(3000)
